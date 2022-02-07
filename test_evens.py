65e93d44d9da027dd16e3286b1ae17ab73b8462f
John-McPherson <john-mac1989@hotmail.co.uk>
2022-02-07 12:14:01 +0000

    add test case to check to see if an empty string returns false

--- test_evens.py
+++ test_evens.py
@@ -6,4 +6,7 @@ class TestEvens(unittest.TestCase):
     def test_throws_error_if_value_passed_is_not_list(self):
         self.assertRaises(TypeError, even_number_of_evens,4)
 
+    def test_values_in_list(self):
+        self.assertEqual(even_number_of_evens([]), False)
+
 unittest.main()
